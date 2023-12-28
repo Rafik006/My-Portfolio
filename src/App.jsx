39d55c3ed05be707a54d7e26b7b0a0c3f9e09597
Rafik006 <rafikmhadhbi01@gmail.com>
2023-12-28 11:37:59 +0100

    version to depoly

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import NavBar from "./components/NavBar";
 import Body from "./components/Body";
 import Landing_Page from "./components/LandingPage/Landing_Page";
 
-
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [animateOut, setAnimateOut] = useState(false);
 
- 
   const getStart = () => {
     setAnimateOut(true);
     setTimeout(() => {
@@ -18,7 +16,7 @@ export default function App() {
   };
   const landingPageClass = `landing-page ${animateOut ? "zoom-out" : ""}`;
   return (
-    <div >
+    <div>
       <NavBar onStartClick={getStart} />
       {isLoading ? (
         <Landing_Page onStartClick={getStart} className={landingPageClass} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,112 @@
-import React from "react";
+// import React from "react";
 
-const NavBar = ({onStartClick}) => {
+// const NavBar = ({onStartClick}) => {
+//   return (
+//     <header className="w-full  h-28 flex items-center justify-around border-b-2 shadow-lg sticky top-0 bg-white z-10">
+//       <img
+//         src="https://www.bobolink.co/storage/news/March2019/Logo.png"
+//         className="h-28 w-28  cursor-pointer"
+//         onClick={onStartClick}
+//       />
+//     <ul className="flex w-[450px] h-28 justify-between items-center font-[500] text-2xl    ">
+//         <li className="cursor-pointer hover:text-primaryColor"><a href="#about_me">Home</a></li>
+//         <li className="cursor-pointer hover:text-primaryColor"><a href="#">Portfolio</a></li>
+//         <li className="cursor-pointer hover:text-primaryColor"><a href="#about_me">About me</a></li>
+//         <li className="cursor-pointer hover:text-primaryColor"><a href="#experience">Experiences</a></li>
+//     </ul>
+//     <button className="font-[600]  text-xl border-solid border-2 border-primaryColor p-4 rounded-xl hover:bg-black hover:text-white transition-all duration-300 ease-in-out"><a href="mailto:rafikmhadhbi01@gmail.com">Contact Me</a> </button>
+//     </header>
+//   );
+// };
+
+// export default NavBar;
+import React, { useState } from "react";
+
+const NavBar = ({ onStartClick }) => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
   return (
-    <header className="w-full  h-28 flex items-center justify-around border-b-2 shadow-lg sticky top-0 bg-white z-10">
-      <img
-        src="https://www.bobolink.co/storage/news/March2019/Logo.png"
-        className="h-28 w-28  cursor-pointer"
-        onClick={onStartClick}
-      />
-    <ul className="flex w-[450px] h-28 justify-between items-center font-[500] text-2xl    ">
-        <li className="cursor-pointer hover:text-primaryColor"><a href="#about_me">Home</a></li>
-        <li className="cursor-pointer hover:text-primaryColor"><a href="#">Portfolio</a></li>
-        <li className="cursor-pointer hover:text-primaryColor"><a href="#about_me">About me</a></li>
-        <li className="cursor-pointer hover:text-primaryColor"><a href="#experience">Experiences</a></li>
-    </ul>
-    <button className="font-[600]  text-xl border-solid border-2 border-primaryColor p-4 rounded-xl hover:bg-black hover:text-white transition-all duration-300 ease-in-out"><a href="mailto:rafikmhadhbi01@gmail.com">Contact Me</a> </button>
-    </header>
+    <nav class="bg-white text-center fixed w-full z-10  top-0 start-0 border-b-2 shadow-lg">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a
+          href="https://flowbite.com/"
+          class="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img
+            src="https://www.bobolink.co/storage/news/March2019/Logo.png"
+            className="h-[100px] w-28  cursor-pointer"
+            onClick={onStartClick}
+          />
+        </a>
+        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          {/* <button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Get started
+          </button> */}
+           <button
+          className="font-[600]  text-xl text-white border-solid border-2 border-primaryColor p-4 rounded-xl bg-black hover:bg-white hover:text-black  hover:p-[10px] transition-all duration-300 ease-in-out"
+          onClick={onStartClick}
+        >
+          Get Started
+        </button>
+          <button
+            onClick={toggleMenu}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded={isMenuOpen}
+          >
+            <span class="sr-only">Open main menu</span>
+            <svg
+              class="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+  <div
+  className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? 'block' : 'hidden'}`}
+  id="navbar-sticky"
+>
+  <ul className="flex flex-col md:flex-row md:space-x-4 p-4 md:p-0 mt-4 font-[500] text-2xl  ">
+    <li className="cursor-pointer hover:text-primaryColor">
+      <a href="#" aria-current="page">
+        Home
+      </a>
+    </li>
+    <li className="cursor-pointer hover:text-primaryColor">
+      <a href="#">About</a>
+    </li>
+    <li className="cursor-pointer hover:text-primaryColor">
+      <a href="#">Services</a>
+    </li>
+    <li className="cursor-pointer hover:text-primaryColor">
+      <a
+        href="#"
+      >
+        Contact
+      </a>
+    </li>
+  </ul>
+</div>
+
+      </div>
+    </nav>
   );
 };
 
