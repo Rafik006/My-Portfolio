dc8f1346d83cdd6e5aee565787985b3939c3e0a5
Rafik006 <rafikmhadhbi01@gmail.com>
2024-01-01 21:56:03 +0100

    fix build bug

--- src/animation/template.jsx
+++ src/animation/template.jsx
@@ -1,28 +1,31 @@
-import AnimatedText from "react-animated-text-content";
+// import AnimatedText from "react-animated-text-content";
 
 import React from "react";
 const Template = ({ info, className,type}) => {
   return (
-    <AnimatedText
-      type={type}  // animate words or chars
-      animation={{
-        x: "200px",
-        y: "-20px",
-        scale: 1.1,
-        ease: "ease-in-out",
-      }}
-      animationType="float"
-      interval={0.06}
-      duration={0.8}
-      tag="div"
-      className={`${className} animated-paragraph`}
+    // <AnimatedText
+    //   type={type}  // animate words or chars
+    //   animation={{
+    //     x: "200px",
+    //     y: "-20px",
+    //     scale: 1.1,
+    //     ease: "ease-in-out",
+    //   }}
+    //   animationType="float"
+    //   interval={0.06}
+    //   duration={0.8}
+    //   tag="div"
+    //   className={`${className} animated-paragraph`}
      
-      includeWhiteSpaces
-      threshold={0.2}
-      rootMargin="10%"
-    >
-      {info}
-    </AnimatedText>
+    //   includeWhiteSpaces
+    //   threshold={0.2}
+    //   rootMargin="10%"
+    // >
+    //   {info}
+    // </AnimatedText>µ
+    <div >
+{info}
+    </div>
   );
 };
 
