0092cdfd0d155445cd81940c4b68da7bf30a9903
Rafik006 <rafikmhadhbi01@gmail.com>
2024-01-02 13:18:43 +0100

    remove animation library

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio/",
   server: {
     open: '/index.html',
     port: 3116, //< -change this
   }
+  
 })
