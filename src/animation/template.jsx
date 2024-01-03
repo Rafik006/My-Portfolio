83bcb8b72052d97869a97494d0f8f3dbc178c784
Rafik006 <rafikmhadhbi01@gmail.com>
2024-01-03 12:11:48 +0100

    updates$

--- src/animation/template.jsx
+++ src/animation/template.jsx
@@ -6,20 +6,19 @@ const Template = ({ info, className,type}) => {
     <AnimatedText
       type={type}  // animate words or chars
       animation={{
-        x: "200px",
-        y: "-20px",
+        x: '200px',
+        y: '-20px',
         scale: 1.1,
-        ease: "ease-in-out",
+        ease: 'ease-in-out',
       }}
       animationType="float"
       interval={0.06}
       duration={0.8}
-      tag="div"
-      className={`${className} animated-paragraph`}
-     
+      tag="p"
+      className="animated-paragraph"
       includeWhiteSpaces
-      threshold={0.2}
-      rootMargin="10%"
+      threshold={0.1}
+      rootMargin="20%"
     >
       {info}
     </AnimatedText>
