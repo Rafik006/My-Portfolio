16f7b73d91d796c3011385a2505f2edf64e7477a
Rafik006 <rafikmhadhbi01@gmail.com>
2024-01-03 12:56:46 +0100

    updates$

--- src/animation/template.jsx
+++ src/animation/template.jsx
@@ -3,35 +3,25 @@ import AnimatedText from "react-animated-text-content";
 import React from "react";
 const Template = ({ info, className, type }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        textAlign: "center",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
+    <AnimatedText
+      type={type} // animate words or chars
+      animation={{
+        x: "200px",
+        y: "-20px",
+        scale: 1.1,
+        ease: "ease-in-out",
       }}
+      animationType="float"
+      interval={0.06}
+      duration={0.8}
+      tag="span"
+      className={`${className} animated-paragraph`}
+      includeWhiteSpaces
+      threshold={0.2}
+      rootMargin="10%"
     >
-      <AnimatedText
-        type="chars"
-        animation={{
-          x: "200px",
-          y: "-20px",
-          scale: 1.5,
-          ease: "ease-in-out",
-        }}
-        animationType="rifle"
-        interval={0.1}
-        duration={0.9}
-        tag="p"
-        className="animated-paragraph"
-        includeWhiteSpaces
-        threshold={0.1}
-        rootMargin="20%"
-      >
-        Welcome To , Beji's Portfolio
-      </AnimatedText>
-    </div>
+      {info}
+    </AnimatedText>
   );
 };
 
